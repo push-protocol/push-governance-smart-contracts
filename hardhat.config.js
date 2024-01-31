46b016e671aebd7e2fa12df32b6412f9d4ebefaf
Md Zartaj Afser <zartaj.afsar@gmail.com>
2024-01-31 16:00:45 +0530

    Initialize

---
+++ hardhat.config.js
@@ -0,0 +1,40 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.6.11",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 99999,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      /*
+        notice no mnemonic here? it will just use account 0 of the buidler node to deploy
+        (you can put in a mnemonic here to set the deployer locally)
+      */
+    },
+
+    // ETH Network
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: {
+        mnemonic: mnemonic(),
+      },
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`, // <---- YOUR INFURA ID! (or it won't work)
+      aaccounts: {
+        mnemonic: mnemonic(),
+      },
+    },
+  },
+};
